a0594c1175ae878d30617d5cac0630304738c781
christopher <christopher@bioinf.uni-leipzig.de>
2024-01-22 15:10:24 +0100

    Tree output to Json, add distance metric

--- seqgen.py
+++ seqgen.py
@@ -11,7 +11,6 @@ import sys
 import subprocess
 import random
 import numpy as np
-import pandas as pd
 import hashlib
 from optparse import OptionParser
 
@@ -23,6 +22,7 @@ import RNA
 from anytree import AnyNode, RenderTree
 from anytree.search import find
 from anytree.iterators.levelorderiter import LevelOrderIter
+from anytree.exporter import JsonExporter
 
 
 __author__ = "Christopher Klapproth"
@@ -57,6 +57,8 @@ mutations_dict = {
     'T': ['A', 'G', 'C', '-'],
     }
 
+mutation_types = ['point', 'deletion', 'insertion', 'crossover']
+
 
 ################################
 #
@@ -76,20 +78,23 @@ def load_aligned_records(filename):
     return records
 
 
-def write_candidate_seqs(seqs, filename):
+def join_to_records(seqs, ids):
     records = []
-    count = 0
-
-    for seq in seqs:
-        count += 1
+    
+    for i in range(0, len(seqs)):
+        seq = seqs[i]
         record = SeqRecord(
         Seq(seq.replace('-', '')),
-        id="candidate_%s" % count,
+        id=ids[i],
         name="",
-        description="synthetic nucleotide sequence %s" % count,
+        description="",
         )
         records.append(record)
         
+    return records
+
+
+def write_candidate_seqs(records, filename):       
     SeqIO.write(records, open(filename, 'w'), format='fasta')
     return 
 
@@ -112,9 +117,8 @@ def map_seq_to_score(aligned_records, scores):
     score_dict = {}
     
     for i in range(0, len(aligned_records)):
-        seq = str(aligned_records[i].seq.ungap('-').ungap('.')).upper().replace('U', 'T')
-        encode = hashlib.md5(seq.encode('utf-8')).hexdigest()
-        score_dict[encode] = scores[i]
+        name = aligned_records[i].id
+        score_dict[name] = scores[i]
     
     return score_dict
 
@@ -158,38 +162,25 @@ def infernal_align(fasta_file, cm_file, cm_scores, cm_alignment):
 
 ################################
 #
-#   Feature calculations
+#   Feature calculation
 #
 ################################
 
 def rna_mfe(node):
     fc = RNA.fold_compound(node.seq)
     mfe_struct, mfe = fc.mfe()
-    node.mfe = mfe
+    return mfe
 
 
 def CG_content(node):
     cg = (node.seq.count('C') + node.seq.count('G')) / len(node.seq)
-    node.CG_content = cg
+    return cg
 
 
-def pairwise_sequence_identity(seq1, seq2):
-    count = 0
-    for i in range(0, len(seq1)):
-        if seq1[i] == seq2[i]:
-            count += 1
-    return count / len(seq1)
-
-
-def mean_pairwise_identity(records):
-    length = len(records)
-    ids = []
-    
-    for i in range(0, length-1):
-        for j in range(i+1, length):
-            ids.append(pairwise_sequence_identity(records[i].seq, records[j].seq))
-    
-    return np.mean(ids)
+def hamming_distance(s1, s2):
+    length = len(s1)
+    sum_of_distances = sum(c1 != c2 for c1, c2 in zip(s1, s2))
+    return (sum_of_distances / length)
 
 
 def select_best_scoring(score_dict, fraction):
@@ -234,18 +225,21 @@ def initialize_tree():
 
 def add_children_nodes(seqs, parent, generation, child_count):
     children = []
+    node_ids = []
     
     for i in range(0, len(seqs)):
+        child_count += 1
         name = 's_{generation}_{child}'.format(
             generation=generation,
-            child=child_count+i,
+            child=child_count,
             )
+        node_ids.append(name)
         seq = seqs[i]
         children.append(AnyNode(id=name, seq=seq, 
                                 generation=generation,
                                 hash=hashlib.md5(seq.encode('utf-8')).hexdigest()))
     parent.children = children
-    return child_count+1
+    return node_ids, child_count
 
 
 def get_generation_nodes(node, generation):
@@ -253,11 +247,29 @@ def get_generation_nodes(node, generation):
     return [node for node in iterator]
 
 
+def assign_similarity_scores(nodes, aligned_records):
+    hamming_dict = {}
+    
+    for i in range(0, len(aligned_records)):
+        distances = []
+        
+        for j in range(0, len(aligned_records)):
+            if (i == j):
+                continue
+            distances.append(hamming_distance(aligned_records[i].seq, aligned_records[j].seq))
+        
+        hamming_dict[aligned_records[i].id] = np.mean(distances)
+    
+    for node in nodes:
+        hamming_dist = hamming_dict.get(node.id, 0)
+        node.mean_hamming_distance = hamming_dist
+
+
 def assign_features_to_nodes(nodes, cm_score_dict):
     for node in nodes:
-        rna_mfe(node)
-        CG_content(node)
-        node.cm_score = cm_score_dict.get(node.hash)
+        node.mfe = rna_mfe(node)
+        node.CG_content = CG_content(node)
+        node.cm_score = cm_score_dict.get(node.id)
 
 
 ################################
@@ -275,19 +287,18 @@ def point_mutate(nt):
     return random.SystemRandom().choice(mutations_dict.get(nt, nucleotides))
 
 
-def point_crossover(seq1, seq2):
-    index = np.random.randint(low=0, high=len(seq2))
-    local_nt = seq2[index]
-    return local_nt, index
+def insertion():
+    return random.SystemRandom().choice(nucleotides)
 
 
-def mutate(seqs, tree, generation, mutation_rate, n_children=def_children):
+def mutate(records, tree, generation, mutation_rate, n_children=def_children):
     return_seqs = []
-    N = 0   # Total number of children added to Tree
+    return_ids = []
+    N = 0   # Counter for total number of children added to Tree
     
-    for seq in seqs:
-        hash_ = hashlib.md5(remove_gaps(seq).encode('utf-8')).hexdigest()
-        node = find(tree, lambda n: n.hash == hash_)
+    for record in records:
+        node = find(tree, lambda n: n.id == record.id)
+        seq = str(record.seq)
         mutated_variants = []
         children = 0
         
@@ -296,31 +307,41 @@ def mutate(seqs, tree, generation, mutation_rate, n_children=def_children):
             seq_ls = list(seq)
         
             for i in range(0, mutation_rate):
-                mutation_type = random.SystemRandom().choice(['point', 'crossover'])
+                mutation_type = random.SystemRandom().choice(mutation_types)
+                index = np.random.randint(low=0, high=len(seq_ls))
                 
                 if (mutation_type == 'point'):
-                    index = np.random.randint(low=0, high=len(seq_ls))
                     local_nt = point_mutate(seq_ls[index])
-                
-                if (mutation_type == 'crossover'):
-                    # TODO: Exclude own ident sequence
-                    crossover_seq = random.SystemRandom().choice(seqs)
-                    local_nt, index = point_crossover(seq_ls, crossover_seq)
+                    seq_ls[index] = local_nt
                     
-                seq_ls[index] = local_nt
-            
+                elif (mutation_type == 'deletion'):
+                    del seq_ls[index]
+                
+                elif (mutation_type == 'insertion'):
+                    local_nt = insertion()
+                    seq_ls[index] = local_nt
+                
+                elif (mutation_type == 'crossover'):
+                    # TODO: Exclude own ident sequence
+                    crossover_seq = str(random.SystemRandom().choice(records).seq)
+                    local_nt = crossover_seq[index]
+                    seq_ls[index] = local_nt
+                                 
             # Add the mutated sequence to the pool
             mut_seq = remove_gaps(str().join(seq_ls))
             mutated_variants.append(mut_seq)
             children += 1
-            
+        
         # Update children in Tree
-        N = add_children_nodes(seqs=mutated_variants, parent=node, generation=generation, child_count=N)
+        record_ids, N = add_children_nodes(seqs=mutated_variants, parent=node, 
+                                           generation=generation, child_count=N)
         
         # Append mutated sequences to return List
         return_seqs += mutated_variants
-    
-    return return_seqs
+        return_ids += record_ids
+        
+    return_records = join_to_records(return_seqs, return_ids)    
+    return return_records
 
 
 ################################
@@ -341,12 +362,12 @@ def main():
     
     # Parse out cmd line arguments
     parser.add_option("-o", "--output", action="store", default="", type="string", dest="output", help="Results file to write to. If empty, defaults to stdout.")
+    parser.add_option("-t", "--tree-file", action="store", default="", type="string", dest="treefile", help="Json file to write the resulting tree to.")
     parser.add_option("-g", "--generations", action="store", default=def_generations, type="int", dest="iterations", help="Integer defining number of cycles.")
     parser.add_option("-M", "--model", action="store", default='', type="string", dest="cov_model", help="Path to covariance model used as reference. If none is provided, will be calculated from start population.")
     parser.add_option("-m", "--mutationrate", action="store", default=def_mutation_rate, type="int", dest="mutation_rate", help="Float defining the base mutation chance per sequence and generation.")
     parser.add_option("-s", "--size", action="store", default=def_selection, type="float", dest="selection", help="Generation size to be selected for the next cycle. Can also be a fraction.")
     parser.add_option("-c", "--children", action="store", default=def_children, type="int", dest="children", help="Initial number of children nodes to generate per generation and element (before scoring).")
-    parser.add_option("-l", "--label", action="store", default='RNA', type="string", dest="label", help="Data label for result table.")
     options, args = parser.parse_args()
     
     # Prepare base parameters
@@ -355,7 +376,6 @@ def main():
     mutation_rate = options.mutation_rate
     n_children = options.children
     fraction = options.selection
-    label = options.label
     population_file = 'population.fa'
     cm_file = 'cov_model.cm'
     candidates_file = 'candidates.fa'
@@ -366,34 +386,42 @@ def main():
     # TODO: Deal with U/T in input file
     # TODO: Deal with input duplicates !!
 
-    # Write the base RNA population file
+    # Load the base RNA input file
     records = load_seq_records(aln_file)
+    
+    # Initialize tree & write initial population file
+    root = initialize_tree()
+    ungapped_seqs = [str(r.seq.ungap('-')) for r in records]
+    record_ids, _ = add_children_nodes(seqs=ungapped_seqs, parent=root, generation=0, child_count=0)
+    
+    for i in range(0, len(records)):
+        records[i].id = record_ids[i] 
+
     write_population_file(records, population_file) 
     
-    # Alignment of current population
+    # Alignment of initial population 
     population_aln = clustalw_alignment(fasta_file=population_file)
-    records = load_aligned_records(filename=population_aln)
-    ungapped_seqs = [str(r.seq.ungap('-')) for r in records]
+    aligned_records = load_aligned_records(filename=population_aln)
     
     # Build an initial CM model iff none is provided
     if len(options.cov_model) == 0:
         build_cm_model(fasta_file=population_file, cm_file=cm_file)
     else:
         cm_file = options.cov_model
-        
-    # Initialize tree
-    root = initialize_tree()
-    add_children_nodes(seqs=ungapped_seqs, parent=root, generation=0, child_count=0)
     
     # Score at zero generation (t=0)
     cm_aligned_records = infernal_align(fasta_file=population_file, cm_file=cm_file,
-                                     cm_scores=score_file, cm_alignment=cm_alignment)
+                                        cm_scores=score_file, cm_alignment=cm_alignment)
     scores = parse_cmscores(cm_scores=score_file)
     score_dict = map_seq_to_score(cm_aligned_records, scores=scores)
     
-    # Add values to tree
+    # Add metrics to tree
     children = get_generation_nodes(root, generation=0)
     assign_features_to_nodes(children, cm_score_dict=score_dict)
+    assign_similarity_scores(nodes=children, aligned_records=aligned_records)
+    
+    # Initial aligned population
+    records = aligned_records
     
     
     # Start Generation:
@@ -401,18 +429,13 @@ def main():
         print('Generation #%s' % generation)
         synthetic_population = 0
         
-        # Calculate alignment of current sequence pool
-        population_aln = clustalw_alignment(fasta_file=population_file)
-        
-        # Load current population and mutate sequences
-        records = load_aligned_records(filename=population_aln)
-        sequences = [str(record.seq) for record in records]
-        mutated_seqs = mutate(seqs=sequences, tree=root, generation=generation,
+        # Mutate current population
+        mutated_records = mutate(records=records, tree=root, generation=generation,
                               mutation_rate=mutation_rate,
                               n_children=n_children)
         
         # Write candidates to Fasta:
-        write_candidate_seqs(seqs=mutated_seqs, filename=candidates_file)
+        write_candidate_seqs(records=mutated_records, filename=candidates_file)
         
         # Align mutated entities to CM model and score them
         cm_aligned_records = infernal_align(fasta_file=candidates_file, cm_file=cm_file,
@@ -422,49 +445,56 @@ def main():
         children = get_generation_nodes(root, generation=generation)
         assign_features_to_nodes(children, cm_score_dict=score_dict)
         
-        # Select best candidate(s) and add to current population
-        best_candidates = select_best_scoring(score_dict=score_dict, fraction=fraction)
-        best_hashes = [x[0] for x in best_candidates]
+        # Write current updated population
         records = []
         
         for child in children:
-            if (child.hash not in best_hashes):
-                continue
             sequence=child.seq
             synthetic_population += 1
             synthetic_record = SeqRecord(
             seq=Seq(sequence),
-            id='synthetic_RNA_%s' % synthetic_population,
+            id=child.id,
             name="",
-            description='covariance model score: %s' % child.cm_score,
+            description="",
             )
             records.append(synthetic_record)
-        
-        # Write population file and restart the cycle
+            
         write_population_file(records, population_file)
-        records = []
+        
+        # Calculate alignment of current sequence pool and add similarity scores in Tree
+        population_aln = clustalw_alignment(fasta_file=population_file)
+        aligned_records = load_aligned_records(filename=population_aln)
+        assign_similarity_scores(nodes=children, aligned_records=aligned_records)
+         
+        # Select best candidate(s)
+        selected_records = []
+        best_candidates = select_best_scoring(score_dict=score_dict, fraction=fraction)
+        best_ids = [x[0] for x in best_candidates]
+        
+        for record in aligned_records:
+            if record.id not in best_ids:
+                continue
+            selected_records.append(record)
+        
+        # Set records list to selected subset and restart the cycle
+        records = selected_records
+        aligned_records = []
+        
+        # END OF LOOP
+        
     
     # Test: render tree
     print(RenderTree(root))
     
-    '''
-    data = {
-        'Generation': generations,
-        'Minimum free energy': mean_mfe,
-        'Mean covariance model score': mean_cm_score,
-        'Mean pairwise identity': mean_identity,
-        'Mean CG content': mean_cg,
-        'Label': [label]*len(generations),
-        }
-    df = pd.DataFrame(data=data)
+    # Export tree as JSON
+    exporter = JsonExporter(indent=2, sort_keys=True)
     
-    if len(options.output) == 0:
-        print('\t'.join([c for c in df.columns]))
-        for i in range(0, len(df)):
-            print('\t'.join([df[c].iloc[i] for c in df.columns]))
+    if len(options.treefile) == 0:
+        pass
     else:
-        df.to_csv(options.output, sep='\t')
-    '''  
-
+        with open(options.treefile, 'w') as f:
+            f.write(exporter.export(root))
+    
+    
 if __name__ == '__main__':
     main()
